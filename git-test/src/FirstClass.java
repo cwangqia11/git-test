f5ebb7724703b324c49e5691879c8dc670078af9
wangxu <591659619@qq.com>
2019-02-23 09:51:11 +0800

    把本地工程添加到git

---
+++ git-test/src/FirstClass.java
@@ -0,0 +1,11 @@
+public class FirstClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+    }
+}
